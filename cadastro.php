f6e4f8841542a748f11a1f7dc3eacb745e6b25c8
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-06 11:50:51 -0300

    Testando..

---
+++ cadastro.php
@@ -0,0 +1,218 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro Pessoa Jurídica</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css">
+
+        <link rel="stylesheet" href="css/cadastro.css"> 
+    </head>
+    <body>
+        <div class="container">
+            <div class="form-group my-3">
+                <h2>Novo Cadastro</h2>
+                <a href-="" class="btn btn-secondary">Pessoa Física</a>
+                <a href-="" class="btn btn-danger">Pessoa Jurídica</a>
+            </div>
+            <form action="">
+
+                <!--Nova Div-->
+                <div class="row">
+                    <div class="col">
+
+                        <div class="form-group">
+
+                            <h4 class="border-top-0">Dados da empresa</h4>
+                            <hr class="mt-0">
+
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for= RazaoSocial><strong>Razão Social</strong></label>
+                            <input type="" class="form-control"  id="RazaoSocial" placeholder="Razão Social">
+
+                        </div>
+
+
+
+                    </div>
+
+
+                </div>
+
+                <div class="form-group">
+
+                    <h4 class="border-top-0 text-center">Dados Pessoais</h4>
+
+
+
+                </div>
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="form-group">
+                            <label><strong>* Nome Fantasia</strong></label>
+                            <input  class="form-control" placeholder="Nome Fantasia">
+
+                        </div>
+
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>* CNPJ</strong></label>
+                            <input  class="form-control" placeholder="CNPJ">
+
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>* Inscrição estadual</strong></label>
+                            <input  class="form-control" placeholder="Inscrição estadual">
+
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>Nome completo</strong></label>
+                            <input  class="form-control" placeholder="Nome completo">
+
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>Nascimento</strong></label>
+                            <input  class="form-control" placeholder="dd/mm/aaaa">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="form-group">
+                            <label><strong>* Sexo</strong></label>
+
+                            <select class="form-control">
+                                <option>Selecione</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>* Telefone</strong></label>
+                            <input  class="form-control" placeholder="(xx)xxxx-xxxx">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+
+                    <h4 class="border-top-0 text-center">Acesso ao Site</h4>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="form-group">
+                            <label><strong>* Telefone Alternativo</strong></label>
+                            <input  class="form-control" placeholder="(xx)xxxx-xxxx">
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="form-group">
+
+                            <label><strong>* E-mail</strong></label>
+                            <input  class="form-control" placeholder="usuario@email.com">
+
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>* Confirme o E-mail</strong></label>
+                            <input  class="form-control" placeholder="usuario@email.com">
+                        </div>
+
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label><strong>* Login</strong></label>
+                            <input  class="form-control" placeholder="usuario">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+
+                    <h4 class="border-top-0 text-center">Endereço</h4>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-8">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1"><strong>* Senha</strong></label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1"><strong>* Confirme a Senha</strong></label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label><strong>* CEP</strong></label>
+                            <input  class="form-control" placeholder="xxxxx-xxx">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label><strong>* Endereço</strong></label>
+                            <input  class="form-control" placeholder="Rua, avenida">
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </form>
+        </div>
+
+
+
+
+
+
+
+
+    </body>
+</html>
